d2f4871969165fafb7d384ffe283de1d44a01e3d
Nix-Ai <nikhiljalindarkale055@gmail.com>
2024-07-06 20:22:16 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = 'TOKEN'
-ADMIN_ID = 'YOUR ID'
+API_TOKEN = '7417760175:AAFfmSxlxQqJOrNfnEfyc7N87GMGil2--hI'
+ADMIN_ID = '1618593409'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
